88872579264787fabe2fd26a7166f860c25cbd14
AntonderEroberer <57536346+AntonderDenker@users.noreply.github.com>
2021-01-04 16:13:03 +0100

    start

---
+++ Neuer Ordner/index.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+    <title>Umfrage | Kryptowährung</title>
+    <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+
+<header></header>
+<div class="container">
+    <div class="poll-container">
+        <div class="card text-center">
+            <div class="card-header">
+                Featured
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            <div class="card-footer text-muted">
+                2 days ago
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
