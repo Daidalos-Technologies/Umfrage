dee6bf9ab75674cd257b4c9fb0233ffd97f4bc6a
AntonderEroberer <57536346+AntonderDenker@users.noreply.github.com>
2021-01-12 18:32:43 +0100

    skip

--- Neuer Ordner/umfrage.php
+++ Neuer Ordner/umfrage.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(!isset($_SESSION["result_id"])){
+if (!isset($_SESSION["result_id"])) {
     $hash = md5(time());
     $_SESSION["result_id"] = $hash;
 }
@@ -8,29 +8,31 @@ if(!isset($_SESSION["result_id"])){
 require "init.php";
 
 
-if(isset($_POST["poll_id"]))
-{
-$old_poll = fetch_by_id($_POST["poll_id"]);
-$old_position = $old_poll["position"];
-
-$result = fetch_result_by_user_id($_SESSION["result_id"], $old_poll["id"]);
-
-if($result == false) {
-    add_result($_SESSION["result_id"], $old_poll["id"], $_POST["answer"]);
-}else {
-    update_result($_SESSION["result_id"], $old_poll["id"], $_POST["answer"]);
-}
+if (isset($_POST["poll_id"])) {
 
 
-if($old_poll["jump"] === 0) {
-    $next_poll = $old_poll["jump_to"];
-}else {
-    $next_poll = $old_poll["position"] + 1;
-}
+    $old_poll = fetch_by_id($_POST["poll_id"]);
+    $old_position = $old_poll["position"];
 
-$poll = fetch_by_position($next_poll);
+    $result = fetch_result_by_user_id($_SESSION["result_id"], $old_poll["id"]);
 
-}else {
+    if ($_POST["skip"] != 1) {
+        if ($result == false) {
+            add_result($_SESSION["result_id"], $old_poll["id"], $_POST["answer"]);
+        } else {
+            update_result($_SESSION["result_id"], $old_poll["id"], $_POST["answer"]);
+        }
+    }
+
+    if ($old_poll["jump"] === 0) {
+        $next_poll = $old_poll["jump_to"];
+    } else {
+        $next_poll = $old_poll["position"] + 1;
+    }
+
+    $poll = fetch_by_position($next_poll);
+
+} else {
     $poll = fetch_by_position(1);
 }
 
@@ -62,8 +64,9 @@ $poll_answers = explode("#", $poll["answers"]);
                 <div class="card-body">
                     <h5 class="card-title"><?php echo $poll["title"]; ?></h5>
                     <p class="card-text"><?php echo $poll["content"]; ?></p>
-                    <form class="answers mt-5 text-left" method="post" action="./umfrage.php">
+                    <form class="answers mt-5 text-left" method="post" id="form" action="./umfrage.php">
                         <input hidden name="poll_id" value="<?php echo $poll["id"]; ?>">
+                        <input hidden name="skip" id="skip_inp">
                         <select class="form-select" aria-label="Default select example" name="answer">
                             <option value="" disabled selected="">---</option>
                             <?php $counter = 0;
@@ -71,7 +74,14 @@ $poll_answers = explode("#", $poll["answers"]);
                                 <option value="<?php echo $answer; ?>"><?php echo $answer; ?></option>
                             <?php endforeach; ?>
                         </select>
-                        <button type="submit" class="btn btn-primary mt-4">Weiter</button>
+                        <?php if ($poll["optional"] == 1): ?>
+                            <div class="d-flex justify-content-between align-items-center mt-4">
+                                <button type="button" class="btn btn-secondary" id="skip">Überspringen</button>
+                                <button type="submit" class="btn btn-primary">Weiter</button>
+                            </div>
+                        <?php else: ?>
+                            <button type="submit" class="btn btn-primary mt-4">Weiter</button>
+                        <?php endif; ?>
                     </form>
 
                 </div>
@@ -84,5 +94,17 @@ $poll_answers = explode("#", $poll["answers"]);
 
 
 </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossorigin="anonymous"></script>
+<script type="text/javascript">
+
+    $("#skip").click(function () {
+        $("#skip_inp").val("1");
+        $("#form").submit();
+    })
+
+</script>
 </body>
 </html>
